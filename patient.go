fae508e865a19db398859722c6c8fb25434ddae1
Andy Gregorowicz <andy@mitre.org>
2014-10-03 16:24:53 -0400

    Actually running the tests

--- patient.go
+++ patient.go
@@ -1,6 +1,7 @@
 package hdsfhir
 
 import (
+	"encoding/json"
 	"time"
 )
 
@@ -10,6 +11,21 @@ type Patient struct {
 	UnixBirthTime int64  `json:"birthdate"`
 }
 
-func (p *Patient) BirthTime() Time {
+func (p *Patient) BirthTime() time.Time {
 	return time.Unix(p.UnixBirthTime, 0)
+}
+
+func (p *Patient) ToJSON() []byte {
+	f := map[string]interface{}{
+		"name": map[string][]string{
+			"family": []string{
+				p.LastName,
+			},
+			"given": []string{
+				p.FirstName,
+			},
+		},
+	}
+	json, _ := json.Marshal(f)
+	return json
 }
--- patient_test.go
+++ patient_test.go
@@ -5,30 +5,43 @@ import (
 	"github.com/pebbe/util"
 	. "gopkg.in/check.v1"
 	"io/ioutil"
+	"testing"
+	"time"
 )
 
 type HDSSuite struct {
 	JSONBlob *[]byte
 }
 
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
 func (s *HDSSuite) SetUpSuite(c *C) {
 	data, err := ioutil.ReadFile("./fixtures/john_peters.json")
 	util.CheckErr(err)
-	s.JSONBlob = data
+	s.JSONBlob = &data
 }
 
 var _ = Suite(&HDSSuite{})
 
 func (s *HDSSuite) TestExtractPatient(c *C) {
 	patient := Patient{}
-	err := json.Unmarshal(s.JSONBlob, &patient)
+	err := json.Unmarshal(*s.JSONBlob, &patient)
 	util.CheckErr(err)
 	c.Assert("John", Equals, patient.FirstName)
 }
 
 func (s *HDSSuite) TestBirthTime(c *C) {
 	patient := Patient{}
-	err := json.Unmarshal(s.JSONBlob, &patient)
+	err := json.Unmarshal(*s.JSONBlob, &patient)
 	util.CheckErr(err)
-	c.Assert(2, Equals, patient.BirthTime())
+	c.Assert(time.February, Equals, patient.BirthTime().Month())
+}
+
+func (s *HDSSuite) TestToJSON(c *C) {
+	patient := Patient{}
+	err := json.Unmarshal(*s.JSONBlob, &patient)
+	util.CheckErr(err)
+	data := patient.ToJSON()
+	c.Assert(data, NotNil)
 }
