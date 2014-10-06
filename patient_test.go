97c73f25872f5e0d4437cb7093d48d3487bc3b1f
Andy Gregorowicz <andy@mitre.org>
2014-10-06 12:00:43 -0400

    Removing the pointer from the slice
    Since the slice is a reference, you don't need a pointer to it...
    Still learning go, so I might be wrong here.

--- patient_test.go
+++ patient_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 type HDSSuite struct {
-	JSONBlob *[]byte
+	JSONBlob []byte
 }
 
 // Hook up gocheck into the "go test" runner.
@@ -19,28 +19,28 @@ func Test(t *testing.T) { TestingT(t) }
 func (s *HDSSuite) SetUpSuite(c *C) {
 	data, err := ioutil.ReadFile("./fixtures/john_peters.json")
 	util.CheckErr(err)
-	s.JSONBlob = &data
+	s.JSONBlob = data
 }
 
 var _ = Suite(&HDSSuite{})
 
 func (s *HDSSuite) TestExtractPatient(c *C) {
 	patient := Patient{}
-	err := json.Unmarshal(*s.JSONBlob, &patient)
+	err := json.Unmarshal(s.JSONBlob, &patient)
 	util.CheckErr(err)
 	c.Assert("John", Equals, patient.FirstName)
 }
 
 func (s *HDSSuite) TestBirthTime(c *C) {
 	patient := Patient{}
-	err := json.Unmarshal(*s.JSONBlob, &patient)
+	err := json.Unmarshal(s.JSONBlob, &patient)
 	util.CheckErr(err)
 	c.Assert(time.February, Equals, patient.BirthTime().Month())
 }
 
 func (s *HDSSuite) TestToJSON(c *C) {
 	patient := Patient{}
-	err := json.Unmarshal(*s.JSONBlob, &patient)
+	err := json.Unmarshal(s.JSONBlob, &patient)
 	util.CheckErr(err)
 	data := patient.ToJSON()
 	c.Assert(data, NotNil)
